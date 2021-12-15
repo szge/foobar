3c5e97b5109bd75f2e32a9aae62ab92b346c8bef
Jonathan <jchiangconan@gmail.com>
2021-12-15 02:23:25 -0500

    added level 4 challenge 2 template

---
+++ c4_2.py
@@ -0,0 +1,10 @@
+def solution(times, time_limit):
+    
+    return None
+
+if __name__ == "__main__":
+    print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
+    # should be [1, 2]
+
+    print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
+    # should be [0, 1]
